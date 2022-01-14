c54fe1e225f48f6a593776519900a12f08394ece
Théo Videira <videira.theo@gmail.com>
2022-01-14 13:58:56 +0100

    hello

---
+++ conf/parsing/nginx_conf/Location.hpp
@@ -0,0 +1,27 @@
+#ifndef LOCATION_HPP
+# define LOCATION_HPP
+# include <iostream>
+# include <vector>
+
+class Location
+{
+private:
+    std::string root;
+    std::string index;
+    std::string autoindex;
+    std::string error_page;
+    size_t client_max_body_size;
+    std::vector<std::string> methods;
+public:
+    Location( void );
+    ~Location();
+};
+
+Location::Location( void ) {
+}
+
+Location::~Location() {
+}
+
+
+#endif 
---
+++ conf/parsing/nginx_conf/ServerInfo.hpp
@@ -0,0 +1,30 @@
+#ifndef SERVER_INFO_HPP
+# define SERVER_INFO_HPP
+# include <iostream>
+# include <vector>
+# include "Location.hpp"
+
+class ServerInfo
+{
+private:
+    std::string listen;
+    std::string root;
+    std::string index;
+    std::string autoindex;
+    std::string error_page;
+    std::string server_name;
+    size_t client_max_body_size;
+    std::vector<Location::Location> locations;
+public:
+    ServerInfo( void );
+    ~ServerInfo();
+};
+
+ServerInfo::ServerInfo( void ) {
+}
+
+ServerInfo::~ServerInfo() {
+}
+
+
+#endif 
