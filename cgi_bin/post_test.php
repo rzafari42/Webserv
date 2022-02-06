b8fc87620142b7a092b18d18b523a7dbc12c7a10
rzafari42 <redwane.zafari@hotmail.com>
2022-02-06 18:32:01 +0100

    PHP script: Error: Undefined array key lastName, firstName and message

---
+++ cgi_bin/post_test.php
@@ -0,0 +1,7 @@
+#!/usr/local/bin/php
+<html>
+    <body>
+        Welcome <?php echo $_POST["lastName"]; $_POST["firstName"]; ?>
+        Message is: <?php echo $_POST["message"]; ?>
+    </body>
+</html>
