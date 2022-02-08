a601beb448f9905a63617dda5f458da42883851c
MrSbarre <simon.sbarre@gmail.com>
2022-02-08 21:41:26 +0100

    post working

--- cgi_bin/post_test.php
+++ cgi_bin/post_test.php
@@ -2,10 +2,11 @@
 <html>
     <body>
         <?php
-            parse_str(getenv('QUERY_STRING'), $_POST);
+            $input_data = file_get_contents("php://stdin");
+            parse_str($input_data, $_POST);
             echo "POST <br>";
-            
-            echo "Welcome " . $_POST["lastName"] . " " . $_POST["firstName"] . "<br>"; 
+
+            echo "Welcome " . $_POST["lastName"] . " " . $_POST["firstName"] . "<br>";
             echo "Your message is: " . $_POST["message"];
         ?>
     </body>
