f73ad2b2d99fc83d6685e356eefc27ff5ad656fc
Redwane ZAFARI <rzafari@e2r11p12.clusters.42paris.fr>
2022-02-13 23:26:45 +0100

    php scrip havec enne a bit changed and /r/n checking added to parser

--- cgi_bin/get_test.php
+++ cgi_bin/get_test.php
@@ -1,12 +1,4 @@
 #!/usr/bin/php-cgi
 <html>
-    <body>
-        <?php
-            parse_str(getenv('QUERY_STRING'), $_GET);
-            echo "GET <br>";
-            
-            echo "Welcome " . $_GET["lastName"] . " " . $_GET["firstName"] . "<br>"; 
-            echo "Your message is: " . $_GET["message"];
-        ?>
-    </body>
+    <body><?php echo "GET<br>"; echo "Welcome " . $_GET["lastName"] . " " . $_GET["firstName"] . "<br>";  echo "Your message is:" . $_GET["message"];?></body>
 </html>
