3e2b83c2a3e5d736e4375f607dc2f2216b8538ac
root <root@DESKTOP-UTTDR80.localdomain>
2022-02-14 19:03:22 +0100

    upload working, needs some more tweaks

--- cgi_bin/upload.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include <stdio.h>      /* printf */
-#include <stdlib.h> 
-
-int main() {
-    std::cout << "MAGNIFIQUE = " << getenv("BODY") << std::endl;
-    return 0;
-}
